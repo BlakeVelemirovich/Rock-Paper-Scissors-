86719093f42412fabdb5c146cf65652be499be69
DragunovVelimirovic <blake_v1993@hotmail.com>
2022-02-24 16:52:35 -0400

    function to let computer select a play finished.

---
+++ game.js
@@ -0,0 +1,11 @@
+function computerPlay {
+		
+	let choices = ['rock', 'paper', 'scissors'];
+	let computerSelection = choices[Math.floor(Math.random() * choices.length)];
+	return computerSelection;
+}
+
+function singleRound(playerSelection, computerSelection) {
+	
+	
+}
