3d0f8661137a40f9919e183006f4ae51545cd89b
DragunovVelimirovic <blake_v1993@hotmail.com>
2022-03-07 13:51:20 -0400

    Buttons, flavour text, and win checker added

--- game.js
+++ game.js
@@ -1,5 +1,8 @@
-let playerWin = 0;
-let computerWin = 0;
+let playerWin = 5;
+let computerWin = 5;
+const playerLives = document.querySelector('.playerLives');
+const computerLives = document.querySelector('.computerLives');
+const heraldText = document.querySelector('.heraldText');
 
 function computerPlay() {
 		
@@ -8,25 +11,52 @@ function computerPlay() {
 	return computerSelection;
 }
 
-function singleRound() {
+function singleRound(playerSelection) {
 	
-	let playerSelection = prompt("Rock, Paper, or Scissors?: ");
 	let computerSelection = computerPlay();
-	
+
+	// If player wins, computer loses one life
 	if ((playerSelection === "rock" && computerSelection === "scissors") || (playerSelection === "paper" && computerSelection === "rock") || (playerSelection === "scissors" && computerSelection === "paper")) {
-		playerWin ++;
+		computerWin --;
+		computerLives.textContent = `Computer lives: ${computerWin}`;
+		heraldText.textContent = 'The rogue mage is struck!'
 		console.log("Player wins round!");
 	}
 
+	// If computer wins, player loses one life
 	else if ((computerSelection === "rock" && playerSelection === "scissors") || (computerSelection === "paper" && playerSelection === "rock") || (computerSelection === "scissors" && playerSelection === "paper")) {
-		computerWin ++;
+		playerWin --;
+		playerLives.textContent = `Player lives: ${playerWin}`;
 		console.log("Computer wins round!");
 	}
 	else {
-		console.log("Tie!");
+		heraldText.textContent = 'Spells clash, mana is burned but for now both casters remain unharmed! Manifest your art quickly lest you be the canvas';
+		console.log("Tie!"); 
+	} 
+
+	// Player wins when the computer has 0 lives
+	if (computerWin === 0) {
+		const victorySpeech = document.createTextNode(' They now grovel at your feet, no doubt hoping for an inkling of your mercy');
+		heraldText.appendChild(victorySpeech);
+	}
+
+	// Computer wins when the player has 0 lives
+	if (playerWin === 0) {
+		const lossSpeech = document.createTextNode(' And now your strength wanes... The art of war has failed! Apperate quickly while there is still time or face the consequences of your actions!');
+		heraldText.appendChild(lossSpeech);
 	}
 }
 
+let buttonPlayerChoice = document.querySelectorAll('button');
+
+buttonPlayerChoice.forEach(button => {
+	button.addEventListener('click', function() {
+		singleRound(button.value);
+	});
+});
+
+
+// Old function for playing 5 rounds in the console before UI update
 /* function game() {
 
 	for (let i = 0; i < 5; i++) {
